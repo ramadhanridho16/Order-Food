3245140e27454a28bc065a81a60bd052089e2044
ridho <ramadhan.ridho.16@students.unnes.ac.id>
2024-04-20 13:49:13 +0700

    Adding models to menu folder

--- project/menu/admin.py
+++ project/menu/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Menus, Promos, Medias, Home_banners
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Menus)
+admin.site.register(Promos)
+admin.site.register(Medias)
+admin.site.register(Home_banners)
--- project/menu/models.py
+++ project/menu/models.py
@@ -10,3 +10,27 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Promos(models.Model):
+    name = models.CharField(max_length=255)
+    start_date = models.DateTimeField(auto_now_add=False)
+    end_date = models.DateTimeField(auto_now_add=False)
+
+class Medias(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    size = models.IntegerField()
+    media = models.ImageField(upload_to='menus_image', blank=True, null=True)
+
+class Home_banners(models.Model):
+    name = models.CharField(max_length=255)
+    media_id = models.ManyToManyField(Medias, related_name='banner')
+
+class Menus(models.Model):
+    name = models.CharField(max_length=255)
+    category_id = models.ForeignKey(Category, related_name='menus', on_delete=models.CASCADE)
+    price = models.CharField(max_length=255)
+    promo_price = models.CharField(max_length=255)
+    promo = models.BooleanField(default=False)
+    promo_id = models.ForeignKey(Promos, related_name='menus', on_delete=models.CASCADE)
+    media_id = models.ManyToManyField(Medias, related_name='menus')
